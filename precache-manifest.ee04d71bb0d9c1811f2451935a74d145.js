2c947c04d1335ca62e1880489e56c0a8660a2b91
Amit Joshi <amjoshi@twilio.com>
2020-05-24 13:46:02 +0300

    Updates

---
+++ precache-manifest.ee04d71bb0d9c1811f2451935a74d145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94aac0dcd64c1b01639fa78f490d911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "296ffddd564d81123d1d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24553c4ef2cba6c68bb8",
+    "url": "/static/css/main.266f6cb9.chunk.css"
+  },
+  {
+    "revision": "296ffddd564d81123d1d",
+    "url": "/static/js/2.137e9fa0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.137e9fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24553c4ef2cba6c68bb8",
+    "url": "/static/js/main.1c1c4b2d.chunk.js"
+  },
+  {
+    "revision": "a5ea60d6c88a0d34a412",
+    "url": "/static/js/runtime-main.466ccbfb.js"
+  }
+]);
